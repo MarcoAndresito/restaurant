e6cd977852569f7429ae4a5ef7f5996543455a8b
Andres Vasquez <you@example.com>
2022-07-13 21:40:15 -0400

    clase 9

--- app/Http/Controllers/ComidaController.php
+++ app/Http/Controllers/ComidaController.php
@@ -16,26 +16,67 @@ class ComidaController
     {
         $comida = ComidaRepository::Find($id);
         if (empty($comida)) {
-            return json("no se encontro la comida", 404);
+            return json([
+                "status" => "error",
+                "errorType" => "internal error",
+                "errorDescription" => "no se encotro el recurso"
+            ], 404);
         }
         return json($comida);
     }
 
     public function store($comida)
     {
+        $errorList = validate($comida, ["id", "nombre", "precio"]);
+        if (!empty($errorList)) {
+            return json($errorList, 400);
+        }
         ComidaRepository::Save($comida);
-        return json("ok");
+        return json([
+            "status" => "ok",
+            "descripcion" => "guardado correctamente"
+        ]);
     }
 
     public function update($comida)
     {
+        $errorList = validate($comida, ["id", "nombre"]);
+        if (!empty($errorList)) {
+            return json($errorList, 400);
+        }
+        if (empty(ComidaRepository::Find($comida->id))) {
+            return json([
+                "status" => "error",
+                "errorType" => "internal error",
+                "errorDescription" => "no se encontro el recurso para editarlo"
+            ], 404);
+        }
         ComidaRepository::Update($comida);
-        return json("ok");
+        return json([
+            "status" => "ok",
+            "descripcion" => "editado correctamente",
+        ]);
     }
 
     public function destroy($id)
     {
+        if (empty($id)) {
+            return json([
+                "status" => "error",
+                "error" => "El 'id' es requerido"
+            ]);
+        }
+        if (empty(ComidaRepository::Find($id))) {
+            return json([
+                "status" => "error",
+                "errorType" => "internal error",
+                "errorDescription" => "no se encontro el recurso para eliminarlo"
+            ], 404);
+        }
         ComidaRepository::Delete($id);
-        return json("ok");
+        return json([
+            "status" => "ok",
+            "descripcion" => "eliminado correctamente",
+        ]);
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,24 @@ function json($data = [], $status = 200)
     $response = new \App\Http\Response($data, $status);
     return $response;
 }
+
+function validate($objet, $valiadateList)
+{
+    $errorList = [];
+
+    foreach ($valiadateList as $value) {
+
+        if (empty($objet->$value)) {
+            array_push($errorList, "el campo " . $value . " es obligatorio");
+        }
+    }
+
+    if (!empty($errorList)) {
+        return [
+            "errorType" => "validation",
+            "errorList" => $errorList
+        ];
+    } else {
+        return null;
+    }
+}
